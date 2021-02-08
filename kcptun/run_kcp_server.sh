f3c834068390226b6997a48a8dd6ab6c6626b605
tagyangyang <tagyangyang@gmail.com>
2021-02-08 08:08:55 +0000

    init

---
+++ kcptun/run_kcp_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./server_linux_amd64 \
+-t 127.0.0.1:12012 \
+-l :29900 \
+-mode fast3 \
+-nocomp \
+-sockbuf 16777217 \
+-dscp 46 \
+-key zhangyue123!@#
---
+++ kcptun/run_ss_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/bin/python \
+/usr/bin/ssserver \
+-c /etc/shadowsocks-python/config.json \
+-d start
